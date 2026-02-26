1ba4064752fab9f5f5b866752ee22e1bbd3920bf
Ye Lin Aung <me@yelinaung.com>
2026-02-26 22:58:35 +0800

    fix: initialize zerolog to stdout with startup log

--- cmd/csy-helper-bot/main.go
+++ cmd/csy-helper-bot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"strings"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -15,6 +16,9 @@ func main() {
 		level = zerolog.InfoLevel
 	}
 	zerolog.SetGlobalLevel(level)
+	zerolog.TimeFieldFormat = time.RFC3339
+	log.Logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
+	log.Info().Str("level", zerolog.GlobalLevel().String()).Msg("Logger initialized")
 
 	if err := appbot.Run(); err != nil {
 		log.Fatal().Err(err).Msg("Bot stopped")
